aa9ae13e692e1b277dfe15c3b50a509b2827c869
shanmukasagar05 <shanmukasagar2021@gmail.com>
2025-02-13 12:40:17 +0530

    port is changed

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.get('/', (req, res) => {
 })
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
